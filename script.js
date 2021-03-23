5ac18b16cc0a61b685db684fdc4a64bff0e98227
Christine Contreras <christineec23@yahoo.com>
2021-03-23 17:51:25 -0500

    functions working in inches

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-console.log('hello world!');
-
-//frame specs pull in
-const numFrames = document.getElementById('numFrames').value;
-
-const frameHeight = parseFloat(document.getElementById('height').value);
-
-const frameWidth = parseFloat(document.getElementById('width').value);
-
-const hookTop = parseFloat(document.getElementById('hookTop').value);
-
-
 //answer divs pull in
 const formAnswers = document.getElementById('form-answers'),
-numFramesAnswer = document.getElementById('numFrameAnswer'),
-frame1 = document.getElementById('frame1'),
-frame2 = document.getElementById('frame2'),
-frame3 = document.getElementById('frame3'),
-widthAnswer1 = document.getElementById('widthAnswer1'),
-widthAnswer2 = document.getElementById('widthAnswer2'),
-widthAnswer3 = document.getElementById('widthAnswer3'),
-heightAnswer = document.getElementsByClassName('heightAnswer');
+    numFramesAnswer = document.getElementById('numFramesAnswer'),
+    frame1 = document.getElementById('frame1'),
+    frame2 = document.getElementById('frame2'),
+    frame3 = document.getElementById('frame3'),
+    widthAnswer1 = document.getElementById('widthAnswer1'),
+    widthAnswer2 = document.getElementById('widthAnswer2'),
+    widthAnswer3 = document.getElementById('widthAnswer3'),
+    heightAnswer = document.getElementsByClassName('heightAnswer');
+
 
 
 function hangFrame() {
+    const frameHeight = parseFloat(document.getElementById('height').value);
+
+    const frameWidth = parseFloat(document.getElementById('width').value);
+
+    const hookTop = parseFloat(document.getElementById('hookTop').value);
+
+    //frame specs pull in
+    const numFrames = parseFloat(document.getElementById('numFrames').value);
+
+    console.log(`number of frames: ${numFrames}`);
+
     // find out height to put in hook
     //height of frame/2
     const eyeHeight = 60; //60 inches
@@ -30,11 +30,12 @@ function hangFrame() {
     //60 inches + half frame height - hook position
     const hangNailHeight = (eyeHeight + middleHeight) - hookTop;
 
+    console.log(`hang nail height: ${hangNailHeight}`);
+
 
     //find middle of wall
     const wallWidth = parseFloat(document.getElementById('widthWall').value);
     const wallMiddle = wallWidth / 2;
-    console.log(wallMiddle);
 
     // check where on the wall's width to hang nails based on how many frames they are hanging
     if (numFrames === 3) {
@@ -48,7 +49,19 @@ function hangFrame() {
         const rightFrame = wallMiddle + frameWidth + 1;
 
         //show all 3 frames in answers
-        
+        numFramesAnswer.innerHTML = "3";
+        formAnswers.classList.add('show');
+        frame1.classList.add('show');
+        frame2.classList.add('show');
+        frame3.classList.add('show');
+        heightAnswer[0].innerHTML = hangNailHeight;
+        heightAnswer[1].innerHTML = hangNailHeight;
+        heightAnswer[2].innerHTML = hangNailHeight;
+        widthAnswer1.innerHTML = leftFrame;
+        widthAnswer2.innerHTML = middleFrame;
+        widthAnswer3.innerHTML = rightFrame;
+
+        console.log('three frames working');
 
 
     } else if (numFrames === 2) {
@@ -59,11 +72,38 @@ function hangFrame() {
         //right frame: middle of wall + 1/2 width of frame + 0.5 inches
         const rightFrame = wallMiddle + (frameWidth / 2) + 0.5;
 
+        //show 2 frames in answers
+        numFramesAnswer.innerHTML = "2";
+        formAnswers.classList.add('show');
+        frame1.classList.add('show');
+        frame2.classList.add('show');
+        frame3.classList.remove('show');
+        heightAnswer[0].innerHTML = hangNailHeight;
+        heightAnswer[1].innerHTML = hangNailHeight;
+        widthAnswer1.innerHTML = leftFrame;
+        widthAnswer2.innerHTML = rightFrame;
+
+        console.log('two frames working');
+
     } else {
+        // alert(numFrames);
         //only hanging 1 frame
         //middle of the wall measurement
         const middleFrame = wallMiddle;
-    }   
+
+        //show 1 frame in answers
+        numFramesAnswer.innerHTML = "1";
+        formAnswers.classList.add('show');
+        frame1.classList.add('show');
+        frame2.classList.remove('show');
+        frame3.classList.remove('show');
+        heightAnswer[0].innerHTML = hangNailHeight;
+        widthAnswer1.innerHTML = middleFrame;
+
+        console.log('one frame working');
+    }
+
+
 
 } //end function
 
@@ -76,3 +116,17 @@ if (document.getElementById('inches').checked) {
     //need to convert to cm
 
 }
+
+
+document.getElementById('formSubmit').addEventListener('click', function () {
+    hangFrame();
+    console.log('click working');
+
+});
+
+
+const form = document.getElementById("form");
+function handleForm(event) { 
+    event.preventDefault(); 
+} 
+form.addEventListener('submit', handleForm);
